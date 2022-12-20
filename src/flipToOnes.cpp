eaa17af65f7aeb17c9bdf4984a9fcbbb7f67ae08
hang-yin <yinhang0226@gmail.com>
2022-12-20 17:02:19 -0600

    Improved space complexity by getting rid of one of the vectors

--- src/flipToOnes.cpp
+++ src/flipToOnes.cpp
@@ -3,32 +3,45 @@
 
 using namespace std;
 
-void FlipToOnes(vector<vector<int>> &matrix, int m, int n) {
-    // Create a vector to store the row and column indices where 1 is found
-    vector<int> rowIndices, colIndices;
+void flipToOnes(vector<vector<int>> &matrix) {
+    // Get the number of rows and columns in the matrix
+    int m = matrix.size();
+    int n = matrix[0].size();
 
-    // Iterate through the matrix and store the indices where 1 is found
+    // Create a flag to store whether the first row or column should be set to 1
+    bool firstRow = false;
+    bool firstCol = false;
+
+    // Iterate through the matrix and set the first row and column flags if necessary
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
             if (matrix[i][j] == 1) {
-                rowIndices.push_back(i);
-                colIndices.push_back(j);
+                if (i == 0) firstRow = true;
+                if (j == 0) firstCol = true;
+                matrix[0][j] = 1;
+                matrix[i][0] = 1;
             }
         }
     }
 
-    // Iterate through the row and column indices and set all the elements in the corresponding rows and columns to 1
-    for (int i = 0; i < rowIndices.size(); i++) {
-        int row = rowIndices[i];
-        for (int j = 0; j < n; j++) {
-            matrix[row][j] = 1;
+    // Iterate through the matrix and set the rows and columns to 1 as necessary
+    for (int i = 1; i < m; i++) {
+        for (int j = 1; j < n; j++) {
+            if (matrix[i][0] == 1 || matrix[0][j] == 1) {
+                matrix[i][j] = 1;
+            }
         }
     }
 
-    for (int i = 0; i < colIndices.size(); i++) {
-        int col = colIndices[i];
-        for (int j = 0; j < m; j++) {
-            matrix[j][col] = 1;
+    // Set the first row and column to 1 if necessary
+    if (firstRow) {
+        for (int j = 0; j < n; j++) {
+            matrix[0][j] = 1;
+        }
+    }
+    if (firstCol) {
+        for (int i = 0; i < m; i++) {
+            matrix[i][0] = 1;
         }
     }
 }
@@ -44,7 +57,7 @@ int main() {
     matrix[1][2] = 1;
 
     // Set the row and column of the element with value 1 to 1
-    FlipToOnes(matrix, 4, 4);
+    flipToOnes(matrix);
 
     // Print the modified matrix
     for (int i = 0; i < 4; i++) {
--- test/flipToOnes_test.cpp
+++ test/flipToOnes_test.cpp
@@ -3,32 +3,45 @@
 
 using namespace std;
 
-void flipToOnes(vector<vector<int>> &matrix, int m, int n) {
-    // Create a vector to store the row and column indices where 1 is found
-    vector<int> rowIndices, colIndices;
+void flipToOnes(vector<vector<int>> &matrix) {
+    // Get the number of rows and columns in the matrix
+    int m = matrix.size();
+    int n = matrix[0].size();
 
-    // Iterate through the matrix and store the indices where 1 is found
+    // Create a flag to store whether the first row or column should be set to 1
+    bool firstRow = false;
+    bool firstCol = false;
+
+    // Iterate through the matrix and set the first row and column flags if necessary
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
             if (matrix[i][j] == 1) {
-                rowIndices.push_back(i);
-                colIndices.push_back(j);
+                if (i == 0) firstRow = true;
+                if (j == 0) firstCol = true;
+                matrix[0][j] = 1;
+                matrix[i][0] = 1;
             }
         }
     }
 
-    // Iterate through the row and column indices and set all the elements in the corresponding rows and columns to 1
-    for (int i = 0; i < rowIndices.size(); i++) {
-        int row = rowIndices[i];
-        for (int j = 0; j < n; j++) {
-            matrix[row][j] = 1;
+    // Iterate through the matrix and set the rows and columns to 1 as necessary
+    for (int i = 1; i < m; i++) {
+        for (int j = 1; j < n; j++) {
+            if (matrix[i][0] == 1 || matrix[0][j] == 1) {
+                matrix[i][j] = 1;
+            }
         }
     }
 
-    for (int i = 0; i < colIndices.size(); i++) {
-        int col = colIndices[i];
-        for (int j = 0; j < m; j++) {
-            matrix[j][col] = 1;
+    // Set the first row and column to 1 if necessary
+    if (firstRow) {
+        for (int j = 0; j < n; j++) {
+            matrix[0][j] = 1;
+        }
+    }
+    if (firstCol) {
+        for (int i = 0; i < m; i++) {
+            matrix[i][0] = 1;
         }
     }
 }
@@ -41,7 +54,7 @@ TEST(FlipToOnesTest, ZeroMatrix) {
                                   {0, 0, 0, 0}};
 
     // Set the row and column of the element with value 1 to 1
-    flipToOnes(matrix, 4, 4);
+    flipToOnes(matrix);
 
     // Check that the matrix is still all zeros
     for (int i = 0; i < 4; i++) {
@@ -59,7 +72,7 @@ TEST(FlipToOnesTest, OneElement) {
                                   {0, 0, 0, 0}};
 
     // Set the row and column of the element with value 1 to 1
-    flipToOnes(matrix, 4, 4);
+    flipToOnes(matrix);
 
     // Check that the row and column of the element with value 1 are set to 1
     for (int i = 0; i < 4; i++) {
@@ -81,7 +94,7 @@ TEST(FlipToOnesTest, MultipleElements) {
                                   {0, 1, 0, 0}};
 
     // Set the row and column of the element with value 1 to 1
-    flipToOnes(matrix, 4, 4);
+    flipToOnes(matrix);
 
     // Create the resulting matrix
     vector<vector<int>> result = {{0, 1, 1, 0},
